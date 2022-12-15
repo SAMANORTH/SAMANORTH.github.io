5e174957c15ef35ba404b799f9afc6569aa8ad38
SAMANORTH <northsama@outlook.com>
2022-12-15 09:02:40 +0800

    Site updated: 2022-12-15 09:02:26

--- js/plugins/fcircle.js
+++ js/plugins/fcircle.js
@@ -1,4 +1,4 @@
-const sitesjs = {
+const FCircle = {
   requestAPI: (url, callback, timeout) => {
     let retryTimes = 5;
     function request() {
@@ -42,24 +42,30 @@ const sitesjs = {
     }
     request();
   },
-  layout: (cfg) => {
+  layoutDiv: (cfg) => {
     const el = $(cfg.el)[0];
     $(el).append('<div class="loading-wrap"><svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-dasharray="60" stroke-dashoffset="60" stroke-opacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"/></path><path stroke-dasharray="15" stroke-dashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></g></svg></div>');
-    sitesjs.requestAPI(cfg.api, function(data) {
+    FCircle.requestAPI(cfg.api, function(data) {
       $(el).find('.loading-wrap').remove();
-      const arr = data.content;
+      const arr = data.article_data || [];
+      const limit = el.getAttribute('limit');
       arr.forEach((item, i) => {
-        var cell = '<div class="site-card">';
-        cell += '<a class="card-link" target="_blank" rel="external nofollow noopener noreferrer" href="' + item.url + '">';
-        cell += '<img src="' + (item.screenshot || ('https://screenshot-api.xaoxuu.com/api?url=' + item.url + '&width=1280&height=720')) + '" onerror="javascript:this.src=\'' + cfg.screenshot + '\';"/>';
-        cell += '<div class="info">';
-        cell += '<img src="' + (item.avatar || cfg.avatar) + '" onerror="javascript:this.src=\'' + cfg.avatar + '\';"/>';
-        cell += '<span class="title">' + item.title + '</span>';
-        cell += '<span class="desc">' + (item.description || item.url) + '</span>';
+        if (limit && i >= limit) {
+          return;
+        }
+        var cell = '<div class="timenode" index="' + i + '">';
+        cell += '<div class="header">';
+        cell += '<div class="user-info">';
+        cell += '<img src="' + (item.avatar || cfg.avatar) + '" onerror="javascript:this.src=\'' + cfg.avatar + '\';">';
+        cell += '<span>' + item.author + '</span>';
         cell += '</div>';
+        cell += '<p>' + item.created + '</p>';
+        cell += '</div>';
+        cell += '<a class="body" href="' + item.link + '" target="_blank" rel="external nofollow noopener noreferrer">';
+        cell += item.title;
         cell += '</a>';
         cell += '</div>';
-        $(el).find('.group-body').append(cell);
+        $(el).append(cell);
       });
     }, function() {
       $(el).find('.loading-wrap svg').remove();
@@ -70,7 +76,7 @@ const sitesjs = {
 }
 
 $(function () {
-  const els = document.getElementsByClassName('stellar-sites-api');
+  const els = document.getElementsByClassName('stellar-fcircle-api');
   for (var i = 0; i < els.length; i++) {
     const el = els[i];
     const api = el.getAttribute('api');
@@ -78,11 +84,9 @@ $(function () {
       continue;
     }
     var cfg = new Object();
-    cfg.class = el.getAttribute('class');
     cfg.el = el;
     cfg.api = api;
-    cfg.avatar = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/link/8f277b4ee0ecd.svg';
-    cfg.screenshot = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/cover/76b86c0226ffd.svg';
-    sitesjs.layout(cfg);
+    cfg.avatar = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/avatar/round/3442075.svg';
+    FCircle.layoutDiv(cfg);
   }
 });
--- js/plugins/weibo.js
+++ js/plugins/weibo.js
@@ -1,4 +1,4 @@
-const sitesjs = {
+const weibojs = {
   requestAPI: (url, callback, timeout) => {
     let retryTimes = 5;
     function request() {
@@ -42,24 +42,54 @@ const sitesjs = {
     }
     request();
   },
-  layout: (cfg) => {
+  layoutDiv: (cfg) => {
     const el = $(cfg.el)[0];
     $(el).append('<div class="loading-wrap"><svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-dasharray="60" stroke-dashoffset="60" stroke-opacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"/></path><path stroke-dasharray="15" stroke-dashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></g></svg></div>');
-    sitesjs.requestAPI(cfg.api, function(data) {
+    weibojs.requestAPI(cfg.api, function(data) {
       $(el).find('.loading-wrap').remove();
-      const arr = data.content;
+      const arr = data.tweets || [];
+      const limit = el.getAttribute('limit');
       arr.forEach((item, i) => {
-        var cell = '<div class="site-card">';
-        cell += '<a class="card-link" target="_blank" rel="external nofollow noopener noreferrer" href="' + item.url + '">';
-        cell += '<img src="' + (item.screenshot || ('https://screenshot-api.xaoxuu.com/api?url=' + item.url + '&width=1280&height=720')) + '" onerror="javascript:this.src=\'' + cfg.screenshot + '\';"/>';
-        cell += '<div class="info">';
-        cell += '<img src="' + (item.avatar || cfg.avatar) + '" onerror="javascript:this.src=\'' + cfg.avatar + '\';"/>';
-        cell += '<span class="title">' + item.title + '</span>';
-        cell += '<span class="desc">' + (item.description || item.url) + '</span>';
+        if (limit && i >= limit) {
+          return;
+        }
+        var cell = '<div class="timenode" index="' + i + '">';
+        cell += '<div class="header">';
+        cell += '<div class="user-info">';
+        cell += '<img src="' + (data.user.avatar_hd || cfg.avatar) + '" onerror="javascript:this.src=\'' + cfg.avatar + '\';">';
+        cell += '<span>' + data.user.nick_name + '</span>';
         cell += '</div>';
+        cell += '<p>' + item.created_at + '</p>';
+        cell += '</div>';
+        cell += '<div class="body">';
+        cell += '<a class="body" href="' + item.url + '" target="_blank" rel="external nofollow noopener noreferrer">';
+        cell += item.content;
+        cell += '</a>';
+        // cell += '</div>';
+        // 每条微博的右下角 转发 评论 点赞
+        cell += '<div class="footer">';
+        cell += '<div class="flex left">';
+        cell += '</div>';
+        cell += '<div class="flex right">';
+        cell += '<div class="item reaction repost">';
+        cell += '<a class="item comments last" href="' + item.url + '#issuecomment-new" target="_blank" rel="external nofollow noopener noreferrer">';
+        cell += '<span>' + '🔗' + ' ' + item.reposts_count + '</span>';
         cell += '</a>';
         cell += '</div>';
-        $(el).find('.group-body').append(cell);
+        cell += '<a class="item comments last" href="' + item.url + '#issuecomment-new" target="_blank" rel="external nofollow noopener noreferrer">';
+        cell += '<span><svg t="1666270368054" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2528" width="200" height="200"><path d="M952 64H72C32.3 64 0 96.3 0 136v508c0 39.7 32.3 72 72 72h261l128 128c14 14 32.5 21.1 50.9 21.1s36.9-7 50.9-21.1l128-128h261c39.7 0 72-32.3 72-72V136c0.2-39.7-32.1-72-71.8-72zM222 462c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72-32.2 72-72 72z m290-7.7c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72c0 39.7-32.2 72-72 72z m290 8c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72c0 39.7-32.2 72-72 72z" p-id="2529"></path></svg> '
+         + (item.comments_count || 0) + '</span>';
+        cell += '</a>';
+        cell += '<div class="item reaction attitudes">';
+        cell += '<a class="item comments last" href="' + item.url + '#issuecomment-new" target="_blank" rel="external nofollow noopener noreferrer">';
+        cell += '<span>' + '👍' + ' ' + item.attitudes_count + '</span>';
+        cell += '</a>';
+        cell += '</div>';
+
+        cell += '</div>';
+        cell += '</div>';
+        // 右下角结束
+        $(el).append(cell);
       });
     }, function() {
       $(el).find('.loading-wrap svg').remove();
@@ -70,19 +100,17 @@ const sitesjs = {
 }
 
 $(function () {
-  const els = document.getElementsByClassName('stellar-sites-api');
+  const els = document.getElementsByClassName('stellar-weibo-api');
   for (var i = 0; i < els.length; i++) {
     const el = els[i];
-    const api = el.getAttribute('api');
+    const api = el.getAttribute('api');       // 这个API可以返回微博的json文件
     if (api == null) {
       continue;
     }
     var cfg = new Object();
-    cfg.class = el.getAttribute('class');
     cfg.el = el;
     cfg.api = api;
-    cfg.avatar = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/link/8f277b4ee0ecd.svg';
-    cfg.screenshot = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/cover/76b86c0226ffd.svg';
-    sitesjs.layout(cfg);
+    cfg.avatar = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/avatar/round/3442075.svg';
+    weibojs.layoutDiv(cfg);
   }
 });
